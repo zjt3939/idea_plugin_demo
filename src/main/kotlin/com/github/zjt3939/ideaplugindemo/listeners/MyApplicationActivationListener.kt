8ef552e3c27034ceb8d325727e8eae57b452ca81
GitHub Action <action@github.com>
2024-10-27 01:40:23 +0000

    Template cleanup

--- src/main/kotlin/com/github/zjt3939/ideaplugindemo/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/zjt3939/ideaplugindemo/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.zjt3939.ideaplugindemo.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
--- src/main/kotlin/com/github/zjt3939/ideaplugindemo/services/MyProjectService.kt
+++ src/main/kotlin/com/github/zjt3939/ideaplugindemo/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.zjt3939.ideaplugindemo.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.zjt3939.ideaplugindemo.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
